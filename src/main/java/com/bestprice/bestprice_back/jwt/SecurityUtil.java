19c01da87ab483402bcb9c4866ae009e88c357e0
이효진 <rabbit3456@naver.com>
2024-11-15 06:24:03 +0900

    getCurrentMemberId

--- src/main/java/com/bestprice/bestprice_back/jwt/SecurityUtil.java
+++ src/main/java/com/bestprice/bestprice_back/jwt/SecurityUtil.java
@@ -6,12 +6,11 @@ import org.springframework.security.core.context.SecurityContextHolder;
 
 
 public class SecurityUtil {
-    public static Long getCurrentMemberId() {
+    public static String getCurrentMemberId() { // String으로 반환
         final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (authentication == null || authentication.getName().equals("anonymousUser")) {
             throw new IllegalStateException("유저 인증 정보가 없습니다. 다시 로그인 해주세요.");
         }
-        return Long.parseLong(authentication.getName()); // String을 Long으로 변환
+        return authentication.getName(); // 그대로 반환
     }
-    
 }
