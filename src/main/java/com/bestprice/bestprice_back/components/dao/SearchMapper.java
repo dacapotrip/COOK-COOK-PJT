1177e8f4a88cea1d40a7ec36d8f7f3b1758b2b19
cukwak <31978182+cukwak@users.noreply.github.com>
2024-11-19 14:12:36 +0900

    Update SearchMapper.java

--- src/main/java/com/bestprice/bestprice_back/components/dao/SearchMapper.java
+++ src/main/java/com/bestprice/bestprice_back/components/dao/SearchMapper.java
@@ -1,10 +1,11 @@
-package com.bestprice.bestprice_back.components.dao;
+package com.bestprice.bestprice_back.dao;
 
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
+import com.bestprice.bestprice_back.components.domain.RecipeDto;
 import com.bestprice.bestprice_back.components.domain.ShopSearchDto;
 
 @Mapper
@@ -13,5 +14,9 @@ public interface SearchMapper {
     public void searchResultRow(@Param("params") List<ShopSearchDto> params);
 
     public List<ShopSearchDto> existChk(String query);
+
+    public List<RecipeDto> allRecipes();
+
+    public List<RecipeDto> getRecipe(String query);
 
 }
